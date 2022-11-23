4a789728a9abe173235a39f5166b0e96431ffcb0
t-bello7 <bello4aus@gmail.com>
2022-11-23 18:34:33 +0100

    - add post_count column to users - add likes_count and comments_count column to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_234634) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_173137) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_234634) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
+    t.integer "comments_count"
+    t.integer "likes_count"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
@@ -48,6 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_234634) do
     t.string "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "posts_count"
   end
 
   add_foreign_key "comments", "posts"

e71edd8fc9ee81d28a98abf2a89ca6f65bf7adde
t-bello7 <bello4aus@gmail.com>
2022-11-24 13:13:34 +0100

    add increment method to likes on post

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,9 @@
 class Like < ApplicationRecord
     belongs_to :author, class_name: "User"
     belongs_to :post
-
+     
     def posts_likes_counter
-        self.posts.likes_count = self.post.likes.length
+        post.increment(:likes_count)
     end
+
 end

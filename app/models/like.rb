231970c8b7d27bc78e2786e78c166d4f6be7c718
t-bello7 <bello4aus@gmail.com>
2022-11-24 15:13:36 +0100

    - Add rubocop linter check to project - Add rspec to test to models and their methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,8 @@
 class Like < ApplicationRecord
-    belongs_to :author, class_name: "User"
-    belongs_to :post
-     
-    def posts_likes_counter
-        post.increment(:likes_count)
-    end
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
+  def posts_likes_counter
+    post.increment(:likes_count)
+  end
 end
---
+++ spec/model/comment_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe Comment, type: :model do
+  let(:new_user) do
+    User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+  end
+
+  let(:new_post) do
+    Post.new(author: new_user, title: 'Hello', text: 'This is my first post')
+  end
+
+  subject { Comment.new(post: new_post.id, author: new_user, text: 'Hi Tom!') }
+  before { subject.save }
+
+  describe 'Association' do
+    it { should belong_to(:author) }
+    it { should belong_to(:post) }
+  end
+
+  describe 'Validation' do
+    it 'Text should not be empty' do
+      subject.text = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'Text should not exceed 100 characters' do
+      subject.text = 'a' * 101
+      expect(subject).to_not be_valid
+    end
+  end
+end

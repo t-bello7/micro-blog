e6cc43ff7fcb6a926da7dc8a31b1ccfdab760197
t-bello7 <bello4aus@gmail.com>
2022-12-15 06:32:04 +0100

    - users can login and register account

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Current.user.posts.new(post_params)
+    @post = current_user.posts.new(post_params)
     if @post.save
       redirect_to root_path
     else

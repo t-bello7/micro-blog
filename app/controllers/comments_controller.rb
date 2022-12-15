66eb8d4d8bd3502b7704e8e9edbf5b16a3b81dfb
t-bello7 <bello4aus@gmail.com>
2022-12-15 04:48:26 +0100

    - add views for devise controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(text: comment_params[:text], author: Current.user)
